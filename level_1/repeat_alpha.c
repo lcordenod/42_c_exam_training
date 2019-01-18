53813b23c553fb818209535418624a5e382b91d7
Lucas CORDENOD <lcordeno@e3r10p2.42.fr>
2019-01-18 17:38:32 +0100

    Finished level 1 - Ok

--- level_1/repeat_alpha.c
+++ level_1/repeat_alpha.c
@@ -6,23 +6,61 @@
 /*   By: lcordeno <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 10:04:40 by lcordeno          #+#    #+#             */
-/*   Updated: 2019/01/18 10:46:12 by lcordeno         ###   ########.fr       */
+/*   Updated: 2019/01/18 16:33:39 by lcordeno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	*repeat_alpha(char *str)
+int		is_letter(char c)
+{
+	if (c <= 'Z' && c >= 'A')
+		return (1);
+	else if (c <= 'z' && c >= 'a')
+		return (1);
+	return (0);
+}
+
+int		is_cap(char letter)
+{
+	if (letter <= 'Z' && letter >= 'A')
+		return (1);
+	return (0);
+}
+
+int		count_repeat(char letter)
 {
 	int i;
-	char *str2;
 
 	i = 0;
-	while (str[i])
+	if (is_cap(letter))
+		i = (int)letter - 64;
+	else
+		i = (int)letter - 96;
+	return (i);
+}
+
+void	repeat_alpha(char *str)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (str[a])
 	{
-		if (str[i] => 'a' && str[i] =< 'z')
+		if (is_letter(str[a]))
+		{
+			b = 0;
+			while (b < count_repeat(str[a]))
+			{
+				write(1, &str[a], 1);
+				b++;
+			}
+		}
+		else
+			write(1, &str[a], 1);
+		a++;
 	}
-	return (str2);
 }
 
 int		main(int ac, char **av)
